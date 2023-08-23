8430fa40bf71c12e2359de7f6d5113d4c09e3d1a
Mabrur Syamhur <imbomabrur0906@gmail.com>
2023-08-24 07:14:11 +0800

    Setup Nuxt Module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['~/assets/css/tailwind.css'],
+  css: ["~/assets/css/tailwind.css"],
   devtools: { enabled: true },
   postcss: {
     plugins: {
@@ -8,4 +8,11 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-})
+  modules: [
+    "@nuxtjs/eslint-module",
+    "@nuxt/image",
+    "@nuxt/content",
+    "nuxt-icon",
+    "nuxt-simple-sitemap",
+  ]
+});
