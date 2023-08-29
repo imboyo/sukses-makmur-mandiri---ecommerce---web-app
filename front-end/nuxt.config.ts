a18df640733b2a1cc11e43d012a6a9a6d5bb15e4
Mabrur Syamhur <imbomabrur0906@gmail.com>
2023-08-29 21:30:39 +0800

    Reorganizing Project

---
+++ front-end/nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: ["~/assets/css/tailwind.css", "vue3-carousel/dist/carousel.css"],
+  devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: [
+    "@nuxt/image",
+    "nuxt-icon",
+    "nuxt-simple-sitemap",
+    "nuxt-lodash",
+    "@pinia/nuxt",
+  ],
+  runtimeConfig: {
+    public: {
+      API_URL: process.env.API_URL || "http://localhost:7000",
+    },
+  }
+});
